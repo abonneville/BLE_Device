633c896fb3461c84982c742d1c21c0d7750c6154
Andrew Bonneville <andrew.bonneville@gmail.com>
2019-11-02 15:11:57 -0700

    Initial commit

---
+++ CppUTest-Target/Src/Startup.cpp
@@ -0,0 +1,213 @@
+/*
+ * Copyright (C) 2019 Andrew Bonneville.  All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+
+extern "C" {
+#include "stm32wbxx_hal.h"
+#include "core_cm4.h"
+}
+
+#include <cstdio>
+#include <cstring>
+#include <errno.h>
+
+
+#include "CppUTest/CommandLineTestRunner.h"
+#include "CppUTest/TestHarness.h"
+#include "CppUTest/TestPlugin.h"
+#include "CppUTest/TestRegistry.h"
+
+
+/* Typedef -----------------------------------------------------------*/
+
+/* Define ------------------------------------------------------------*/
+
+/* Macro -------------------------------------------------------------*/
+
+/* Variables ---------------------------------------------------------*/
+
+
+/* Function prototypes -----------------------------------------------*/
+static void PrepNewlib( void );
+
+
+/* External functions ------------------------------------------------*/
+
+
+/**
+ * @brief Test/checks performed here run pre/post each and every test case
+ */
+class BaseTest : public TestPlugin {
+public:
+	BaseTest(const SimpleString& name)
+	: TestPlugin(name)
+	{
+		/* Runs one-time before any testing is performed
+		 */
+	}
+
+	~BaseTest()
+	{
+		/* Runs one-time after all testing has completed
+		 */
+	}
+
+	virtual void preTestAction(UtestShell & /*test*/, TestResult & /*result*/)
+	{
+		/* Runs before each and every test case */
+
+
+		errno = 0;
+	}
+
+	void postTestAction(UtestShell& test, TestResult& result)
+	{
+		/* Runs after each and every test case */
+
+
+		CHECK_EQUAL(errno, 0);
+
+	}
+protected:
+private:
+};
+
+
+
+
+
+
+
+
+extern "C" void StartApplication()
+{
+	/* Prepare optional library features */
+	PrepNewlib();
+
+	/* Register tests that run before and after every test case */
+	BaseTest bt("BaseTest");
+	TestRegistry::getCurrentRegistry()->installPlugin(&bt);
+
+	const char *argv[] = {"", "-v", NULL };
+	const int argc = sizeof( argv ) / sizeof( char* ) - 1;
+
+	std::printf("\n**** Starting test now! ****\n\n");
+	std::fflush(stdout);
+
+	std::printf("\n Results: %d \n", RUN_ALL_TESTS(argc, argv));
+	std::printf("**** Testing complete! ****\n");
+	std::fflush(stdout);
+
+	while(true){}
+
+}
+
+
+
+
+/**
+ * @brief	By design, the newlib library reduces its default footprint size by not statically
+ * allocating all its RAM requirements during compilation. If and only if an optional feature is
+ * used, then the newlib library acquires the memory via the heap. This behavior impacts unit
+ * testing, because when the memory is acquired for the first time, it is never released back
+ * to the memory pool and results in a "false" memory leak being detected.
+ *
+ * This method is used to stimulate optional features prior to testing, so that the
+ * heap memory is acquired prior to testing.
+ *
+ * @note	This has to be repeated for each test thread, since each thread maintains its own
+ * control structures
+ */
+static void PrepNewlib( void )
+{
+	/* stdio is already addressed when the terminal interface is setup and used for reporting */
+
+	/* strtok() */
+	char str[] = "Brown foxes are cool";
+	char *token = std::strtok( str, " " );
+	volatile int count = 0;
+	while ( token != nullptr )
+	{
+		++count;
+		token = std::strtok( nullptr, " " );
+	}
+
+	/* rand() */
+	std::srand ( count );
+	volatile int randomValue = std::rand();
+	++randomValue;
+
+}
+
+
+
+
+/**
+ * Temporary location until HAL extension layer is to be ported/implemented.
+ */
+
+/**
+ * @brief Redirects stdout to the JTAG serial wire viewer (SWV)
+ * @param fd, not used. File/device ID (stdout = 0)
+ * @param buffer is data to be sent out
+ * @param len is how many bytes to be sent out
+ * @retval number of bytes sent
+ */
+extern "C" int _write(int fd, char *ptr, int len)
+{
+	int DataIdx;
+
+	for (DataIdx = 0; DataIdx < len; DataIdx++)
+	{
+		ITM_SendChar(*ptr++);
+	}
+	return len;
+}
+
+
+/**
+ _sbrk
+ Increase program data space. Malloc and related functions depend on this
+**/
+extern "C" caddr_t _sbrk(int incr)
+{
+	extern char end asm("end");
+	extern char _end asm("_end");
+	const char * const heap_limit =&_end;
+	static char *heap_end;
+	char *prev_heap_end;
+
+	if (heap_end == 0)
+		heap_end = &end;
+
+	prev_heap_end = heap_end;
+	if (heap_end + incr > heap_limit)
+	{
+		errno = ENOMEM;
+		return (caddr_t) -1;
+	}
+
+	heap_end += incr;
+
+	return (caddr_t) prev_heap_end;
+}
+
---
+++ CppUTest-Target/Src/UuidTest.cpp
@@ -0,0 +1,191 @@
+/*
+ * Copyright (C) 2019 Andrew Bonneville.  All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+#include <EffectiveBLE.hpp>
+#include "CppUTest/TestHarness.h"
+
+/* Typedef -----------------------------------------------------------*/
+
+/* Define ------------------------------------------------------------*/
+
+/* Macro -------------------------------------------------------------*/
+
+/* Variables ---------------------------------------------------------*/
+
+/* Function prototypes -----------------------------------------------*/
+
+/* External functions ------------------------------------------------*/
+
+
+TEST_GROUP(Uuid16) {};
+TEST_GROUP(Uuid128) {};
+
+TEST(Uuid16, uint16_t)
+{
+	uint16_t id1 = 0xFF33;
+	ble::Uuid16 uuid1 ( ble::to_uuid16(id1) );
+	ble::Uuid16 uuid2 {0xFF, 0x33};
+	ble::Uuid16 uuid3 ( ble::to_uuid16(0) );
+
+	CHECK( uuid1 == uuid2 );
+	CHECK( uuid1 != uuid3 );
+
+	uuid3 = ble::to_uuid16(9834);
+	CHECK( uuid3[0] == 0x26 );
+	CHECK( uuid3[1] == 0x6A );
+	uuid3 = ble::to_uuid16(76);
+	CHECK( uuid3[0] == 0 );
+	CHECK( uuid3[1] == 76 );
+}
+
+
+TEST(Uuid16, byteArray)
+{
+	const ble::Uuid16 id1 { 127, 0};
+	ble::Uuid16 uuid1 {id1};
+	ble::Uuid16 uuid2 (id1);
+	ble::Uuid16 uuid3 ( ble::to_uuid16(0) );
+	ble::Uuid16 uuid4 {127, 0};
+
+	CHECK( uuid1 == uuid2 );
+	CHECK( uuid1 != uuid3 );
+	CHECK( uuid1 == uuid4 );
+
+	uuid4 = {234, 76};
+	CHECK( uuid4[0] == 234 );
+	CHECK( uuid4[1] == 76 );
+
+	uuid4[1] = 44;
+	CHECK( uuid4[0] == 234 );
+	CHECK( uuid4[1] == 44 );
+
+	uuid4 = {88};
+	CHECK( uuid4[0] == 88 );
+	CHECK( uuid4[1] == 0 );
+}
+
+
+TEST(Uuid16, otherOperators)
+{
+
+	/* size() */
+	ble::Uuid16 uuid1;
+	CHECK( 2 == uuid1.size() );
+
+	/* copy() */
+	uint8_t id1[2] {};
+	uuid1 = {0x7D, 0x63};
+
+	//uuid1.copy( id1 );
+	std::copy(uuid1.begin(), uuid1.end(), id1);
+	CHECK( 0x7D == id1[0] );
+	CHECK( 0x63 == id1[1] );
+
+	/* Verify access to raw pointer */
+	CHECK( uuid1.data() == &uuid1[0] );
+}
+
+
+TEST(Uuid128, byteArray)
+{
+	const ble::Uuid128 id1
+		{
+			127, 0, 22, 33,
+			127, 0, 22, 33,
+			127, 0, 22, 33,
+			127, 0, 22, 33
+		};
+	ble::Uuid128 uuid1 {id1};
+	ble::Uuid128 uuid2 (id1);
+	ble::Uuid128 uuid3 {};
+	ble::Uuid128 uuid4 ({id1});
+
+	CHECK( uuid1 == uuid2 );
+	CHECK( uuid1 != uuid3 );
+	CHECK( uuid1 == uuid4 );
+
+	uuid4 = {234, 76};
+	CHECK( uuid4[0] == 234 );
+	CHECK( uuid4[1] == 76 );
+	CHECK( uuid4[2] == 0  );
+	CHECK( uuid4[3] == 0  );
+	CHECK( uuid4[4] == 0  );
+	CHECK( uuid4[5] == 0  );
+	CHECK( uuid4[6] == 0  );
+	CHECK( uuid4[7] == 0  );
+	CHECK( uuid4[8] == 0  );
+	CHECK( uuid4[9] == 0  );
+	CHECK( uuid4[10] == 0  );
+	CHECK( uuid4[11] == 0  );
+	CHECK( uuid4[12] == 0  );
+	CHECK( uuid4[13] == 0  );
+	CHECK( uuid4[14] == 0  );
+	CHECK( uuid4[15] == 0  );
+
+	uuid4[1] = 44;
+	CHECK( uuid4[0] == 234 );
+	CHECK( uuid4[1] == 44 );
+}
+
+
+TEST(Uuid128, otherOperators)
+{
+
+	/* size() */
+	ble::Uuid128 uuid1;
+	CHECK( 16 == uuid1.size() );
+
+	/* copy() */
+	uint8_t id1[16] {};
+	uuid1 = {125, 99};
+
+	std::copy(uuid1.begin(), uuid1.end(), id1);
+	//uuid1.copy( id1 );
+	CHECK( 125 == id1[0] );
+	CHECK( 99  == id1[1] );
+
+	CHECK( 0   == id1[2] );
+	CHECK( 0   == id1[3] );
+
+	CHECK( 0   == id1[4] );
+	CHECK( 0   == id1[5] );
+
+	CHECK( 0   == id1[6] );
+	CHECK( 0   == id1[7] );
+
+	CHECK( 0   == id1[8] );
+	CHECK( 0   == id1[9] );
+
+	CHECK( 0   == id1[10] );
+	CHECK( 0   == id1[11] );
+
+	CHECK( 0   == id1[12] );
+	CHECK( 0   == id1[13] );
+
+	CHECK( 0   == id1[14] );
+	CHECK( 0   == id1[15] );
+
+	/* Verify access to raw pointer */
+	CHECK( uuid1.data() == &uuid1[0] );
+}
+
+
